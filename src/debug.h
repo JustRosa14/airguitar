a883a54bcc5eb5d37819c40c166e33961cc45211
JustRosa14 <78366746+JustRosa14@users.noreply.github.com>
2024-02-04 21:27:20 +0100

    sorta half broken string things

---
+++ src/debug.h
@@ -0,0 +1,7 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+//#define SHOW_STRUM_DATA
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,21 @@
 
 void visualizeData(bool gyro, bool accel);
 bool analyzeStrum();
-void makeNoise(bool noise);
+int playString(bool noise);
+int determineFret();
 
 accelData accel_data;
 gyroData  gyro_data;
 int lastFourValues[4] = {0, 0, 0, 0};
 Gyro g;
+
+
+int string; 
+int fret;
+int low_E_string[]  = {fE4, fF4, fGb4, fG4, fAb4};
+int A_string[]      = {fA4, fBb4, fB4, fC5, fDb5};
+int D_string[]      = {fD5, fEb5, fE5, fF5, fGb5};
+int G_string[]      = {fG5, fAb5, fA5, fBb5, fB5};
 
 void setup() {
   Serial.begin(9600);
@@ -29,20 +38,42 @@ void setup() {
   pinMode(D7, INPUT_PULLUP);
   pinMode(D4, INPUT_PULLUP);
   pinMode(D3, OUTPUT);
-  
+  string = -1;
+  fret = 0;
   g = Gyro();
-
+  
+  visualizeData(0, 1);
+  visualizeData(0, 1);
 }
 
 void loop() {
   //visualizeData(0, 1);
 
-  makeNoise(analyzeStrum());
+  string = playString(analyzeStrum());
+  fret = determineFret();
+
+  switch(string){
+    case 0:
+      Serial.println("String 0");
+      tone(D3, string1[fret], 200);
+      break;
+    case 1:
+      Serial.println("String 1");
+      tone(D3, string2[fret], 200);
+      break;
+    default:
+      
+      break;
+  }
 
 
 
-  
 
+  // for(int i=0;i<9;i++){
+  // Serial.print(analogRead(A0));
+  // Serial.print(" "); 
+  // }
+  // Serial.print("\n");
   
 
   
@@ -55,21 +86,53 @@ void loop() {
 
 }
 
-void makeNoise(bool noise){
+int determineFret(){
+  int temp = analogRead(A0);
+
+  switch(temp){
+    case 300 ... 400:
+      return 1;
+      break;
+    case 401 ... 500:
+      return 2;
+      break;
+    case 501 ... 600:
+      return 3;
+      break;
+    case 601 ... 700:
+      return 4;
+      break;
+    case 701 ... 800:
+      return 5;
+      break;
+    default:
+      return 0;
+      break;
+  }
+}
+
+int playString(bool noise){
   if(noise){
-      Serial.println("Making noise");
       if(!digitalRead(KNOP4)){
-        tone(D3, fE4, 200);
+        return 5;
+        //tone(D3, fE4, 200);
       }else if(!digitalRead(KNOP3)){
-        tone(D3, fA4, 200);
+        return 4;
+        //tone(D3, fA4, 200);
       }else if(!digitalRead(KNOP2)){
-        tone(D3, fD5, 200);
+        return 3;
+        //tone(D3, fD5, 200);
       }else if(!digitalRead(KNOP1)){
-        tone(D3, fG5, 200);
+        //tone(D3, fG5, 200);
+        return 2;
       }    
       else{
-        tone(D3, fB3, 200);
+        //tone(D3, fB3, 200);
+        return 1;
       }
+    }
+    else{
+      return -1;
     }
 }
 
@@ -82,10 +145,11 @@ bool analyzeStrum(){
   if(accel_data.y < -10000){
     for(int i=0;i<10;i++){
       accel_data = g.getAccelData();
+#ifdef SHOW_STRUM_DATA
       visualizeData(0, 1);
+#endif
       if(accel_data.x < -15000){
         x_condition = true;
-        Serial.println("x_condition Done");
       }
       if(x_condition){
         return true;
